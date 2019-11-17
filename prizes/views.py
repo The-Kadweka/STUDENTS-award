24d0cb0cad3f10943461571afdafb805269ed5f2
Hussein Kadweka <developersjuniors@gmail.com>
2019-11-17 23:05:22 +0300

    added a view function for the dates

--- prizes/views.py
+++ prizes/views.py
@@ -11,23 +11,26 @@ from django.contrib.auth.models import User
 def welcome(request):
     return render(request, 'base.html')
 
-@login_required(login_url='/accounts/login/')
-def new_profile(request):
-    current_user = request.user
-    if request.method == 'POST':
-        form = NewsProfileForm(request.POST, request.FILES)
-        if form.is_valid():
-            profile = form.save(commit=False)
-            profile.username = current_user
-            profile.user_id=current_user.id
-            profile.save()
-        return redirect('profile')
-    else:
-        form = NewsProfileForm()
-    return render(request, 'profile.html', {"form":form})
+def past_days_news(request,past_date):
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
 
-def profile(request):
-    current_user = request.user
-    profile = Profile.objects.get(username=current_user)
-    posts=Image.objects.filter(profile_id=current_user.id)
-    return render(request, 'update_profile.html',{"profile":profile})
+    day = convert_dates(date)
+    html = f'''
+        <html>
+            <body>
+                <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
+
+def past_days_news(request,past_date):
+
+    try:
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
