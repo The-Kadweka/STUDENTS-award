e436083567b0dd6a730c2b053a51b3f68701c4b4
Hussein Kadweka <developersjuniors@gmail.com>
2019-11-18 20:30:41 +0300

    styled the prize template

--- awards/urls.py
+++ awards/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^',include('prizes.urls'))
+    url(r'^',include('prizes.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
 
 ]
