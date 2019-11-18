12153324b3d375bc5e6127a67a579a68f493c6de
Hussein Kadweka <developersjuniors@gmail.com>
2019-11-18 19:02:27 +0300

    create a view function to display the awards categories

--- prizes/urls.py
+++ prizes/urls.py
@@ -5,7 +5,10 @@ from django.conf.urls.static import static
 
 urlpatterns=[
     url('^$',views.index,name = 'welcome'),
-    url(r'^search/', views.search_results, name='search_results')
+    url('^prizes/',views.prizes,name = 'prizes'),
+    url(r'^search/', views.search_results, name='search_results'),
+    url('^today/$',views.awards_of_day,name='awardsToday'),
+    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_news,name = 'pastAwards')
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- prizes/views.py
+++ prizes/views.py
@@ -5,22 +5,58 @@ from .models import Awards,Student
 
 # Create your views here.
 def index(request):
+    date = dt.date.today()
     students = Student.get_all_students()
     awards = Awards.todays_awards()
-    return render(request, 'students.html',{"students":students,'awards':awards})
+    print(date)
+    return render(request, 'students.html',{'date': date,"students":students,'awards':awards})
 
 
+
+def awards_of_day(request):
+    date = dt.date.today()
+
+    # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
+    day = convert_dates(date)
+    html = f'''
+        <html>
+            <body>
+                <h1> News for {day} {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
 
 def convert_dates(dates):
 
     # Function that gets the weekday number for the date.
-     day_number = dt.date.weekday(dates)
+    day_number = dt.date.weekday(dates)
 
-     days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
+    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
 
     # Returning the actual day of the week
-     day = days[day_number]
-     return day
+    day = days[day_number]
+    return day
+
+def past_days_news(request,past_date):
+
+    try:
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
+
+    day = convert_dates(date)
+    html = f'''
+        <html>
+            <body>
+                <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
+
 def search_results(request):
 
     if 'student' in request.GET and request.GET["student"]:
@@ -33,3 +69,6 @@ def search_results(request):
     else:
         message = "You haven't searched for any term"
         return render(request, 'search.html',{"message":message})
+
+def prizes(request):
+    return render(request,'prize.html')
